4e69f0fa94e73cb7d21d1c2474663a88a2e0acd3
nipeshkc7 <nipeshkc7@gmail.com>
2020-06-01 19:33:58 +1000

    Added sample data

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
+const axios = require("axios");
+
 exports.handler = async (event) => {
-    let matches = await getOddsApi();
-    await addMatchesToDB(matches);
-    
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Successfully updated DynamoDB'),
+  let matches = await getUpcomingMatches();
+
+  if (matches === undefined) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Internal Server Error"),
     };
-    return response;
+  }
+
+  await addMatchesToDB(matches);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Successfully updated DynamoDB"),
+  };
 };
 
-const getOddsApi = () => {
-    const ODDS_API_KEY = process.env.ODDS_API_KEY;
-    const URL = 'https://api.the-odds-api.com/v3/odds';
-    const REGION = 'au';
-    // TODO get data from api
+const getUpcomingMatches = async () => {
+  const ODDS_API_KEY = process.env.ODDS_API_KEY;
+  const REGION = "au";
+  const SPORT_KEY = "upcoming";
+  const MARKET_TYPE = "h2h";
+
+  let responseFromOddsAPI;
+
+  try {
+    responseFromOddsAPI = await axios.get(
+      "https://api.the-odds-api.com/v3/odds",
+      {
+        params: {
+          api_key: ODDS_API_KEY,
+          sport: SPORT_KEY,
+          region: REGION, // uk | us | eu | au
+          mkt: MARKET_TYPE, // h2h | spreads | totals
+        },
+      }
+    );
+    if (responseFromOddsAPI.success == false) throw new Error();
+  } catch {
+    console.log("Error fetching data from odds API");
     return;
-}
+  }
+  return responseFromOddsAPI.data;
+};
 
 const addMatchesToDB = () => {
-    // TODO store to dynamoDB
-    return;
-}
+  // TODO store to dynamoDB
+  return;
+};
