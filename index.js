a7648c516e8ca1c7d5bfe6848bf4b02b55c3f12b
nipeshkc7 <nipeshkc7@gmail.com>
2020-06-01 13:24:54 +1000

    Added gitignore

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 exports.handler = async (event) => {
+    let matches = await getOddsApi();
+    await addMatchesToDB(matches);
+    
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github!'),
+        body: JSON.stringify('Successfully updated DynamoDB'),
     };
     return response;
 };
+
+const getOddsApi = () => {
+    const ODDS_API_KEY = process.env.ODDS_API_KEY;
+    const URL = 'https://api.the-odds-api.com/v3/odds';
+    const REGION = 'au';
+    // TODO get data from api
+    return;
+}
+
+const addMatchesToDB = () => {
+    // TODO store to dynamoDB
+    return;
+}
